a0992c288ab028d5c7b7acfc3822069cc7b21aa0
winchifrisky <himardrp@gmail.com>
2023-01-28 16:43:09 +0000

    Making more improvements.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -12,52 +12,27 @@ app.get('/new-route', (req, res) => {
   res.send('This is a new endpoint');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price()),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
+routerApi(app);
 
-app.get('products/filter', (req, res) => {
-  res.send('I am a filter');
-});
+// app.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if (limit && offset) {
+//     res.json({
+//       limit,
+//       offset
+//     });
+//   } else {
+//     res.send('There are no parameters');
+//   }
+// });
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 550
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('There are no parameters');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-})
+// app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//   const { categoryId, productId } = req.params;
+//   res.json({
+//     categoryId,
+//     productId,
+//   });
+// })
 
 app.listen(port, () => {
   console.log('My port');

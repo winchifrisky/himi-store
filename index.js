d70dc817d2f57b80861b0321b79c90aa6aece136
winchifrisky <himardrp@gmail.com>
2023-01-29 17:21:44 +0000

    Reorganizing the project.

--- index.js
+++ index.js
@@ -8,31 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hi, my server in express');
 });
 
-app.get('/new-route', (req, res) => {
-  res.send('This is a new endpoint');
-});
-
 routerApi(app);
-
-// app.get('/users', (req, res) => {
-//   const { limit, offset } = req.query;
-//   if (limit && offset) {
-//     res.json({
-//       limit,
-//       offset
-//     });
-//   } else {
-//     res.send('There are no parameters');
-//   }
-// });
-
-// app.get('/categories/:categoryId/products/:productId', (req, res) => {
-//   const { categoryId, productId } = req.params;
-//   res.json({
-//     categoryId,
-//     productId,
-//   });
-// })
 
 app.listen(port, () => {
   console.log('My port');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-const productsRouter = require('./productsRouter');
-//const usersRouter = require ('./usersRouter');
+const express = require('express');
+
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router')
+const usersRouter = require ('./users.router');
 
 function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router)
   app.use('/products', productsRouter);
+  app.use('/categories', categoriesRouter);
+  app.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
